9a6c517c77d96615529cb0b1e81313d0e0adb029
Arturo Reyes Leal <gizmoh@localhost.localdomain>
2018-06-24 20:59:04 -0400

    final commit

--- Huffman.cpp
+++ Huffman.cpp
@@ -80,7 +80,7 @@ int main(int argc, char** argv){
     t0 = clock();
     /*What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Where does it come from?Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, , comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from  by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.Where can I get some?There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which  look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
     */
-    string cadena = "supercalifragilistico espiralidoso";
+    string cadena = "TresTristesTigresTrigoTrigabanEnUnTrigal";
     for (int i = 0; i < cadena.length(); i++){
         pointer = 0;
         while (pointer < 58){
@@ -150,9 +150,14 @@ int main(int argc, char** argv){
         output = output + diccionario[cadena[i]];
     }
     t1 = clock();
-    cout << "Tiempo de codificacion: " << (double(t1-t0)/CLOCKS_PER_SEC)<< "s" << endl;
     cout << "Texto codificado: " << endl;
     cout<< output <<endl;
+
+    double inSize = cadena.size()*CHAR_BIT;
+    double outSize = output.size();
+    double Ratio = outSize/inSize;
+
+    //Aqui empieza el decodificado.
     Decoder decoder;
     string decoding;
     t2 = clock();
@@ -168,5 +173,9 @@ int main(int argc, char** argv){
     }
     cout << endl;
     t3 = clock();
+    cout << "Tamaño de entrada en bits: " << inSize << endl;
+    cout << "Tamaño de salida en bits: " << outSize << endl;
+    cout << "Proporcion de compresion: " << Ratio << endl;
+    cout << "Tiempo de codificacion: " << (double(t1-t0)/CLOCKS_PER_SEC)<< "s" << endl;
     cout << "Tiempo de decodificacion: " << (double(t3-t2)/CLOCKS_PER_SEC) << "s" << endl;
 }
